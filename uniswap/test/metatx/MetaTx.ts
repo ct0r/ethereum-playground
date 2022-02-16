c6d59d58e97db50bcdca84aacb59564bb336f89f
ct0r <ct0r@protonmail.com>
2022-02-16 20:49:50 +0000

    Nest metatx tests to top level describe

--- uniswap/test/metatx/MetaTx.ts
+++ uniswap/test/metatx/MetaTx.ts
@@ -3,49 +3,51 @@ import { expect } from "chai";
 import { Contract, Signer } from "ethers";
 import { ethers } from "hardhat";
 
-const tx = (result: Promise<TransactionResponse>) =>
-  result.then((response) => response.wait());
+describe("MetaTx", () => {
+  const tx = (result: Promise<TransactionResponse>) =>
+    result.then((response) => response.wait());
 
-describe("metaMint", () => {
-  let metaTx: Contract;
-  let owner: Signer;
-  let signer1: Signer;
+  describe("metaMint", () => {
+    let metaTx: Contract;
+    let owner: Signer;
+    let signer1: Signer;
 
-  beforeEach(async () => {
-    [owner, signer1] = await ethers.getSigners();
+    beforeEach(async () => {
+      [owner, signer1] = await ethers.getSigners();
 
-    metaTx = await (await ethers.getContractFactory("MetaTx"))
-      .deploy()
-      .then((metaTx) => metaTx.deployed());
-  });
+      metaTx = await (await ethers.getContractFactory("MetaTx"))
+        .deploy()
+        .then((metaTx) => metaTx.deployed());
+    });
 
-  it("mints with a valid signature", async () => {
-    const amount = 100;
-    const to = await signer1.getAddress();
-    const hash = ethers.utils.arrayify(
-      ethers.utils.solidityKeccak256(["address", "uint256"], [to, amount])
-    );
+    it("mints with a valid signature", async () => {
+      const amount = 100;
+      const to = await signer1.getAddress();
+      const hash = ethers.utils.arrayify(
+        ethers.utils.solidityKeccak256(["address", "uint256"], [to, amount])
+      );
 
-    const signature = await signer1.signMessage(hash);
+      const signature = await signer1.signMessage(hash);
 
-    await tx(metaTx.metaMint(to, amount, signature));
+      await tx(metaTx.metaMint(to, amount, signature));
 
-    expect(await metaTx.balanceOf(to)).to.equal(amount);
-  });
+      expect(await metaTx.balanceOf(to)).to.equal(amount);
+    });
 
-  it("fails with an invalid signature", async () => {
-    const amount = 100;
-    const to = await signer1.getAddress();
-    const hash = ethers.utils.arrayify(
-      ethers.utils.solidityKeccak256(["address", "uint256"], [to, amount])
-    );
+    it("fails with an invalid signature", async () => {
+      const amount = 100;
+      const to = await signer1.getAddress();
+      const hash = ethers.utils.arrayify(
+        ethers.utils.solidityKeccak256(["address", "uint256"], [to, amount])
+      );
 
-    const signature = await owner.signMessage(hash);
+      const signature = await owner.signMessage(hash);
 
-    await expect(tx(metaTx.metaMint(to, amount, signature))).to.be.revertedWith(
-      "You shall not mint!"
-    );
+      await expect(
+        tx(metaTx.metaMint(to, amount, signature))
+      ).to.be.revertedWith("You shall not mint!");
 
-    expect(await metaTx.balanceOf(to)).to.equal(0);
+      expect(await metaTx.balanceOf(to)).to.equal(0);
+    });
   });
 });
